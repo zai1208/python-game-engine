30c1f371927dd7fde7023b578f0092699b666831
zai1208 <zainkh1432@outlook.com>
2020-08-11 01:42:10 +0000

    Update events.py
    Fixed spelling mistake

--- engine/events.py
+++ engine/events.py
@@ -2,6 +2,6 @@ import tkinter as tk
 
 class event():
     def __init__(self, events, window):
-        assert type(events) == dict, 'events must be a dictionary containing the key \n and function whoch does something when the key is pressed, \nexample: \nevents = {\n"Up", onkeyup\n}'
+        assert type(events) == dict, 'events must be a dictionary containing the key \n and function which does something when the key is pressed, \nexample: \nevents = {\n"Up", onkeyup\n}'
         for event in events:
             window.bind(f"<{event}>", events[event])
