bc1f0b9b9dc40b36e998465d059d53c46418aade
zai1208 <zainkh1432@outlook.com>
2020-08-11 01:40:24 +0000

    Create events.py

---
+++ engine/events.py
@@ -0,0 +1,7 @@
+import tkinter as tk
+
+class event():
+    def __init__(self, events):
+        assert type(events) == dict, 'events must be a dictionary containing the key \n and function whoch does something when the key is pressed, \nexample: \nevents = {\n"Up", onkeyup\n}'
+        for event in events:
+            window.bind(f"<{event}>", events[event])
